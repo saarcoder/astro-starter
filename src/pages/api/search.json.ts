11590770ab06cac76bf1a348314f9aa08557962d
Alex Kimoto <66242241+saarcoder@users.noreply.github.com>
2024-07-27 20:11:17 +0200

    Fetch blog articles from search query with GET handler for API route (exemplary)

---
+++ src/pages/api/search.json.ts
@@ -0,0 +1,28 @@
+import type { APIRoute } from "astro";
+import { getCollection } from "astro:content";
+import type { CollectionEntry } from "astro:content";
+
+export const GET: APIRoute= async ({ url }): Promise<Response>=> {
+    const query: string | null= url.searchParams.get('query');
+    console.log(query);
+    if(!query) return new Response(JSON.stringify({
+        error: 'Search query is required'
+        }), { 
+        status: 400,
+        headers: { 'Content-Type': 'application/json' }
+    });
+    const allBlogArticles: CollectionEntry<'blog'>[]= await getCollection('blog');
+
+    const searchResults= allBlogArticles.filter(article=> {
+        const titleMatch: boolean= article.data.title.toLowerCase().includes(query!.toLowerCase())
+        const contentMatch: boolean= article.body.toLowerCase().includes(query!.toLowerCase())
+        const tagsMatch: boolean= article.data.tags.some(tag=> tag.toLowerCase().includes(query!.toLowerCase()))
+        const slugMatch: boolean= article.slug.toLowerCase().includes(query!.toLowerCase())
+        return titleMatch || contentMatch || tagsMatch || slugMatch
+    });
+    
+    return new Response(JSON.stringify(searchResults), { 
+        status: 200,
+        headers: { 'Content-Type': 'application/json' }
+    });
+}
